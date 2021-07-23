49085a935d0fb3c7649d68a28b99eb2d01d247b8
Alex R <alejandro.herrera@ciitizen.com>
2021-07-22 21:09:29 -0700

    data

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -70,68 +70,23 @@ const Navbar: FC = props => {
                   </div>
                 </div>
                 <div className='absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0'>
-                  {/* Profile dropdown */}
-                  <Menu as='div' className='ml-3 relative'>
-                    {({ open }) => (
-                      <>
-                        <Transition
-                          show={open}
-                          as={Fragment}
-                          enter='transition ease-out duration-100'
-                          enterFrom='transform opacity-0 scale-95'
-                          enterTo='transform opacity-100 scale-100'
-                          leave='transition ease-in duration-75'
-                          leaveFrom='transform opacity-100 scale-100'
-                          leaveTo='transform opacity-0 scale-95'
-                        >
-                          <Menu.Items
-                            static
-                            className='origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none'
-                          >
-                            <Menu.Item>
-                              {({ active }) => (
-                                <a
-                                  href='#'
-                                  className={clsx(
-                                    active ? 'bg-gray-100' : '',
-                                    'block px-4 py-2 text-sm text-gray-700'
-                                  )}
-                                >
-                                  Your Profile
-                                </a>
-                              )}
-                            </Menu.Item>
-                            <Menu.Item>
-                              {({ active }) => (
-                                <a
-                                  href='#'
-                                  className={clsx(
-                                    active ? 'bg-gray-100' : '',
-                                    'block px-4 py-2 text-sm text-gray-700'
-                                  )}
-                                >
-                                  Settings
-                                </a>
-                              )}
-                            </Menu.Item>
-                            <Menu.Item>
-                              {({ active }) => (
-                                <a
-                                  href='#'
-                                  className={clsx(
-                                    active ? 'bg-gray-100' : '',
-                                    'block px-4 py-2 text-sm text-gray-700'
-                                  )}
-                                >
-                                  Sign out
-                                </a>
-                              )}
-                            </Menu.Item>
-                          </Menu.Items>
-                        </Transition>
-                      </>
+                  {/* <button className='bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white'>
+                    <span className='sr-only'>View notifications</span>
+                    <BellIcon className='h-6 w-6' aria-hidden='true' />
+                  </button> */}
+
+                  <a
+                    // href={''}
+                    className={clsx(
+                      'bg-teal-300 text-teal-800 transition-all',
+                      'hover:bg-teal-400 hover:text-teal-900',
+                      // 'border-2 border-teal-900',
+                      'px-3 py-2 rounded-md text-sm font-medium'
                     )}
-                  </Menu>
+                    title='Download my resumé'
+                  >
+                    Resumé
+                  </a>
                 </div>
               </div>
             </div>
