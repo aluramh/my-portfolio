345d8cc10044dbe1c1e7f385a9fcca0c75181e35
Alex R <aluramh@gmail.com>
2021-12-10 11:15:29 -0600

    fixed issue with styled components

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,8 +1,37 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { ServerStyleSheet } from 'styled-components'
+
 class MyDocument extends Document {
+  /**
+   * This is used to collect the styled-components before rendering so
+   * we have a set list of generated classNames and we don't have
+   * syncing issues.
+   * https://styled-components.com/docs/advanced#nextjs
+   */
   static async getInitialProps(ctx: any) {
-    const initialProps = await Document.getInitialProps(ctx)
-    return { ...initialProps }
+    const sheet = new ServerStyleSheet()
+    const originalRenderPage = ctx.renderPage
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App: any) => (props: any) =>
+            sheet.collectStyles(<App {...props} />),
+        })
+
+      const initialProps = await Document.getInitialProps(ctx)
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      }
+    } finally {
+      sheet.seal()
+    }
   }
 
   render() {
